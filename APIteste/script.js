abe9d5b313b16f81f125a1e838427b37c92fd256
Luana Rosa <105086263+LuanaZZRosa@users.noreply.github.com>
2023-08-28 18:08:46 -0300

    Add files via upload

---
+++ APIteste/script.js
@@ -0,0 +1,49 @@
+const formulario = document.querySelector("form");
+const Inome = document.querySelector("nome");
+const Iemail = document.querySelector("email");
+const Isenha = document.querySelector("senha");
+const Itel = document.querySelector("tel");
+
+function cadastrar() {
+
+    fetch ("/http://Localhost: 8080/cadastrar",
+
+    { 
+        
+        Headers:{
+        'accept': 'application/json',
+        'content=Type': 'application/json'
+
+    },
+
+    Method: "POST",
+    body: JSON.stringify({
+        nome: Inome.value,
+        email: Iemail.value,
+        senha: Isenha.value,
+        telefone: Itel.value,
+    })
+    
+
+    })
+    .then(function (res) { console.log(res) })
+    .catch(function (res) {console.log(res)})
+
+};
+
+function limpar () {
+
+    Inome.value= "";
+    Iemail.value= "";
+    Isenha.value= "";
+    Itel.value= "";
+
+};
+
+formulario.addEventListener{'submit', function (event) {
+    event.preventDefault();
+
+   cadastrar();
+   limpar();
+
+}};
